3787d626a7f7084fdfe5450eb22f627c7a0c20bb
Alderik89 <95878035+Alderik89@users.noreply.github.com>
2023-07-03 10:35:40 -0300

    Create heroe.js
    Este apartado es el encargado de la interacción entre botones y textos con el usuario.

---
+++ heroe.js
@@ -0,0 +1,187 @@
+const button1 = document.querySelector('#button1')
+const button2 = document.querySelector('#button2')
+const description = document.querySelector("#description")
+
+
+
+
+button2.addEventListener('click',()=> {
+button2.textContent = ("Esa nunca fue una opcion")
+
+button2.addEventListener('click',()=> {
+button2.textContent = ("Que no es una opcion")
+button2.addEventListener('click',()=> {
+button2.textContent = ("No importa cuanto insistas")
+button2.addEventListener('click',()=> {
+button2.textContent = ("Dale, juga la puta aventura")
+button2.addEventListener('click',()=> {
+button2.textContent = ("Te voy a desactivar el boton si seguis jodiendo")
+button2.addEventListener('click',()=> {
+button2.textContent = ("Listo este boton ya no hace nada clickea hasta el infinito")
+})
+})
+})
+})
+})
+}
+)
+
+button1.addEventListener('click', ()=> {
+  
+
+button1.innerHTML = ("Pelear")
+button2.innerHTML = ("Vine por tu Tesoro")
+
+
+if (button1.textContent === "Pelear" && button2.textContent !== "Salir")
+{
+
+description.innerHTML = "El Dragon con los ojos enfurecidos mira directamente a tu alma y por medio de una telepatia que solo conocen algunos frikis que jugaron Dungeon and Dragons te dice"
+
+question.innerHTML = "Joven aventurero nosotros somos una especie antigua y poderosa llena de conocimiento asi que te lo voy a preguntar una sola vez ¿Que estas haciendo en mi caverna?"
+
+button2.innerHTML = "Vine por tu Tesoro"
+button2.addEventListener('click', ()=> {
+button1.innerHTML = "Acertijo"
+ button2.addEventListener('click', ()=> {
+      
+      description.innerHTML = "Con todo el apenamiento del mundo bajas la mirada, pedis disculpas y te retiras a tu casa para poder jugar League of Legends con campeones como Kayn, Viego o Yasuo que van con tu estilo"
+
+      question.innerHTML = "En la primera partida que arrancas te cruzas al dragon en el quipo contrario y te mata 15 veces al compas de spamear GG YASUO"
+      
+      button1.innerHTML = "Tal vez seas no seas una heroe pero viviste para contar otro dia ¿Fin?"
+
+      button2.remove()
+
+  if (button1 === "Tal vez seas no seas una heroe pero viviste para contar otro dia ¿Fin?")
+
+location.reload()
+button1.addEventListener('click', ()=> {
+  location.reload()
+  
+  })
+      
+     })
+if (button1.textContent === "Acertijo")
+{
+  description.innerHTML = "Sabiendo que no hay chances de ganar un combate cuerpo a cuerpo, decidis contarle la verdad al dragon"
+
+question.innerHTML = "Joven aprendiz de novato, pocas personas declaran sus intenciones en una primera instancia. Pero te voy a dar una posibilidad ya que hoy me siento benevolente para que logres tu cometido. Podes retirarte sin ninguna concecuencia o Resolver mi acertijo para quedarte con tu recompensa "
+  
+  button2.innerHTML = "Irse"
+  button1.addEventListener('click', ()=> {
+    button1.innerHTML = "KonamiCode"
+
+   
+    if (button1.textContent === "KonamiCode")
+    {
+
+      description.innerHTML = "Asustado y confuso al saber que aceptaste el desafio de un Reptil de 20 metros de altura que escupe fuego y es portador de Eones de conocimiento escuchas su pregunta"
+
+    question.innerHTML = "¿Como se llama esta combinacion Arcana forjada en los templos de los tramposos para hacerte la vida mas facil en muchos espacios de ocio digital?"
+
+button1.addEventListener('click', ()=> {
+  
+  description.innerHTML = "Contestas con todo el impetud del mundo Es el LEGENDARIO KONAMI CODE casi que podes sentir como la victoria y sus jugosas joyas estan en tus manos"
+
+  question.innerHTML = "El dragon sin emitir palabra con su pata toma una gema que pesa alrededor de 15kg y la lanza con toda su fuerza sobre tu cabeza, aplastandola de una forma grotesca"
+
+  button1.innerHTML = "Eso es lo que obtienen los cheaters sucios ¿Retry?"
+  
+   button2.remove()
+
+  if (button1 === "Eso es lo que obtienen los cheaters sucios ¿Retry?")
+
+location.reload()
+button1.addEventListener('click', ()=> {
+  location.reload()
+  
+  })
+
+
+})
+
+    button2.innerHTML = "CapcomCode"
+    button2.addEventListener('click', ()=> {
+      button1.innerHTML = "Estas muerto por pensar que capcom no te venderia el codigo como dlc ¿Retry?"
+      
+      if (button1.textContent === "Estas muerto por pensar que capcom no te venderia el codigo como dlc ¿Retry?")
+      button2.remove()
+
+  if (button1 === "Estas muerto por pensar que capcom no te venderia el codigo como dlc")
+
+location.reload()
+button1.addEventListener('click', ()=> {
+  location.reload()
+  
+  })
+      
+      {
+        description.innerHTML = "Sudando por demas mientras pensas cuando van a sacar un remaster de la remake del remaster del ultimo remake de Resident Evil 4 contestas con todo Impetud"
+
+        question.innerHTML = "Podes apreciar la exprecion de decepcion del dragon antes que te decapite con sus poderosas garras."
+
+        
+
+      }
+      })
+    }
+  })
+  
+}
+  })
+  
+button1.addEventListener('click', ()=> {
+
+button1.innerHTML = "Espada y Escudo"
+
+if (button1.textContent === "Espada y Escudo")
+{
+description.innerHTML = "Gracias a que pasaron mas de 60 años del primer libro del Señor de los Anillos, todo aventurero, incluso alguien tan novato como vos siempre tiene a la mano mas de una alternativa de combate"
+
+question.innerHTML = "¿Enserio me vas a atacar tristemente con fuerza bruta? Ya es el cuarto esta semana y lo unico que logran es que mi cueva huela a asado"
+
+button2.innerHTML = "Arco y Flecha"
+button2.addEventListener('click', ()=> 
+{
+description.innerHTML = "sin pensarlo dos veces con la mano izquierda tomas tu arco mientras la mano derecha posiciona la flecha. Tension, posicionamiento, acomodar la flecha Y....."
+
+question.innerHTML = "El inmutable dragon al ver como se resvala la flecha antes que puedas soltar la tension del arco lanza una carcajada que salpica pelotas de fuego sobre tu torzo"
+
+  button1.innerHTML = "Estas muerto ya que toda tu vida te la pasaste desarrollando fuerza y poca destreza  ¿Retry?"
+  
+  button2.remove()
+  if (button1 === "Estas muerto ya que toda tu vida te la pasaste desarrollando fuerza y poca destreza  ¿Retry?")
+
+location.reload()
+button1.addEventListener('click', ()=> {
+  location.reload()
+  
+  })
+
+  })
+button1.addEventListener('click', ()=> {
+  
+  description.innerHTML = "Sacas tu espada y cargando con tu gran escudo arremetes contra el dragon cubriendote de las llamas que te envuelven"
+
+question.innerHTML = "Lamentablemente el fuego que desprende el dragon derrite tu escudo junto con tu espada y por supuesto el resto de tu cuerpo"
+
+button1.innerHTML = "ESTAS MUERTO POR SER UN NOOB IMPULSIVO ¿RETRY?"
+button2.remove()
+if (button1 === "ESTAS MUERTO POR SER UN NOOB IMPULSIVO ¿RETRY?" )
+location.reload()
+button1.addEventListener('click', ()=> {
+  location.reload()
+  
+  })
+
+  })
+
+
+
+
+}
+
+})
+
+}})
